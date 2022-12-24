b0a8797c78c1fa65a167a49ba1219730bd9f69e2
Rohan Kishore <109947257+rohankishore@users.noreply.github.com>
2022-12-24 13:20:47 +0530

    Update ModuleFile.py

--- ModuleFile.py
+++ ModuleFile.py
@@ -1,23 +1,23 @@
 """
 ModuleFile
-
 This file contains all the functions for the features of Aura Notes. This makes the code better accessible and easy to contribute
 for the devs
-
 Translate feature isn't included in this file as the number of lines increases dramatically. That's in a different .py file.
 """
+import json
 import os
 import tkinter
 import webbrowser
+from difflib import get_close_matches
 from idlelib.colorizer import ColorDelegator
 from idlelib.percolator import Percolator
 from tkinter import *
 from tkinter import messagebox, ttk
-from tkinter.filedialog import askopenfilename
 import pyttsx3
 import wikipedia
+import pywhatkit as kit
+import cv2
 import pyperclip
-
 
 def resize(root):
     root.geometry("500x500")
@@ -205,7 +205,7 @@ def find(e, notepad, textfinded):
         while 1:
             # searches for desired string from index 1
             idx = notepad.search(s, idx, nocase=1,
-                                        stopindex=END)
+                                      stopindex=END)
             if not idx:
                 break
 
@@ -335,6 +335,66 @@ def settings():
 
     settings_page.mainloop()
 
+def get_meaning(notepad):
+    data = json.load(open('data.json'))
+    word = notepad.selection_get()
+
+    word = word.lower()
+
+    if word in data:
+        meaning = data[word]
+
+        for item in meaning:
+            messagebox.showinfo('Meaning',  item)
+
+    elif len(get_close_matches(word, data.keys())) > 0:
+
+        close_match = get_close_matches(word, data.keys())[0]
+
+        res = messagebox.askyesno('Confirm', 'Did you mean ' + close_match + ' instead?')
+
+        if res:
+            meaning = data[close_match]
+
+            for item in meaning:
+                messagebox.showinfo('Meaning', item)
+
+    else:
+        messagebox.showerror('Error', 'The word doesnt exist.Please double check it.')
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+def talk_meaning(notepad):
+    data = json.load(open('data.json'))
+    word = notepad.selection_get()
+
+    word = word.lower()
+
+    if word in data:
+        meaning = data[word]
+
+        for item in meaning:
+            speak(item)
+
+    elif len(get_close_matches(word, data.keys())) > 0:
+
+        close_match = get_close_matches(word, data.keys())[0]
+
+        res = messagebox.askyesno('Confirm', 'Did you mean ' + close_match + ' instead?')
+
+        if res:
+            meaning = data[close_match]
+
+            for item in meaning:
+                speak(item)
+
+    else:
+        messagebox.showerror('Error', 'The word doesnt exist.Please double check it.')
+
+
 # wikipedia
 def wiki(notepad):
     global query
@@ -346,7 +406,7 @@ def wiki(notepad):
         messagebox.showerror("Error!", "Select any text or word to continue")
         w.destroy()
 
-    w.configureure(background="#1b1b1b"), w.title("Wikipedia"), w.resizable(False, False)
+    w.configure(background="#1b1b1b"), w.title("Wikipedia"), w.resizable(False, False)
 
     text_w = Text(w, background="#1b1b1b", foreground="white", font=("Helvetica", 12))
     text_w.pack(pady=10)
@@ -362,38 +422,57 @@ def wiki(notepad):
     text_w.insert(1.0, result)
     text_w.configure(state=DISABLED)
 
-def txt_analyze():
-    filepath = str(askopenfilename(filetypes=[("Text files", "*.txt")]))
+def find_replace(notepad):
+    #find function
+    def finnd():
+        word = find_input.get()                         #take input from find_input
+        notepad.tag_remove("match",'1.0',END)
+        matches = 0
+        if word:
+            start_pos = "1.0"
+            while True:
+                start_pos = notepad.search(word,start_pos,stopindex=END) #searching for the word
+                if not start_pos:
+                    break
+                end_pos = f"{start_pos}+{len(word)}c"
+                notepad.tag_add("match",start_pos,end_pos)
+                matches += 1
+                start_pos = end_pos
+                notepad.tag_config("match",foreground = "yellow",background = "#1d1d1d")
+    def replace():
+        word = find_input.get()                 #getting input from variable
+        replace_text = replace_input.get()
+        content = notepad.get(1.0,END)
+        new_content = content.replace(word,replace_text)
+        notepad.delete(1.0,END)
+        notepad.insert(1.0,new_content)
 
-    with open(filepath, 'r+') as f:
-        lines = str(len(f.readlines()))
-        chars = str(len(f.read()))
+    find_dialogue = Toplevel()   #inside find windows
+    find_dialogue.config(background="#1d1d1d")
+    find_dialogue.geometry("450x200")
+    find_dialogue.title("Find/replace")
+    find_dialogue.resizable(0,0)
+    find_frame = LabelFrame(find_dialogue,text = "Find/replace", background="#1d1d1d", foreground="white")
+    find_frame.pack(pady = 20)
 
-    text = "Total Number of Lines: " + lines + "\n" + "Total Character Count: " + chars + "\n"
+    text_find_label = ttk.Label(find_frame, text='Find : ', background="#1d1d1d", foreground="cyan")
+    text_replace_label = ttk.Label(find_frame, text= 'Replace', background="#1d1d1d", foreground="cyan")
 
-    tk = Toplevel()
-    tk.overrideredirect(True)
+    find_input = ttk.Entry(find_frame, width=30, background="grey")
+    replace_input = ttk.Entry(find_frame, width=30)
 
-    tk.configure(background="black")
+    find_button = ttk.Button(find_frame, text='Find',command = finnd)
+    replace_button = ttk.Button(find_frame, text= 'Replace',command = replace)
 
-    w = 300  # width for the Tk root
-    h = 100  # height for the Tk root
+    text_find_label.grid(row=0, column=0, padx=4, pady=4)
+    text_replace_label.grid(row=1, column=0, padx=4, pady=4)
 
-    # get screen width and height
-    ws = tk.winfo_screenwidth()  # width of the screen
-    hs = tk.winfo_screenheight()  # height of the screen
+    find_input.grid(row=0, column=1, padx=4, pady=4)
+    replace_input.grid(row=1, column=1, padx=4, pady=4)
 
-    # calculate x and y coordinates for the Tk root window
-    x = (ws / 2) - (w / 2)
-    y = (hs / 2) - (h / 2)
+    find_button.grid(row=2, column=0, padx=8, pady=20)
+    replace_button.grid(row=2, column=1, padx=8, pady=20)
 
-    # set the dimensions of the screen
-    # and where it is placed
-    tk.geometry('%dx%d+%d+%d' % (w, h, x, y))
+    find_dialogue.mainloop()
 
-    def close():
-        tk.destroy()
 
-    Label(tk, text=text, background="black", foreground="orange").pack()
-
-    ttk.Button(tk, text="OK", command=close).pack(side=BOTTOM)
