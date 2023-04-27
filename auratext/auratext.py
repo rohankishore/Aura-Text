9e2cffa57093a33ef060e0059aba55029f660c19
Rohan Kishore <109947257+rohankishore@users.noreply.github.com>
2023-04-27 13:21:48 +0530

    v2.1

--- auratext/auratext.py
+++ auratext/auratext.py
@@ -1,49 +1,43 @@
-import sys
-import time
-import webbrowser
-from pathlib import Path
-from tkinter import filedialog
+import webbrowser, sys, ModuleFile, time, Preferences, random
 from tkinter import messagebox
-
-import autopep8
-import ModuleFile
-from CodeEditor import CodeEditor
 from PySide6 import QtGui
-from PySide6.QtGui import QFont
-from PySide6.QtGui import QKeySequence
-from PySide6.QtGui import QPixmap
-from PySide6.QtGui import QShortcut
-from PySide6.QtWidgets import QApplication
-from PySide6.QtWidgets import QMainWindow
-from PySide6.QtWidgets import QMessageBox
-from PySide6.QtWidgets import QSplashScreen
+from PySide6.QtWidgets import *
+from PySide6.QtGui import QShortcut, QPixmap, QKeySequence, QFont
 from Syntax_Highlighter import PythonHighlighter
 from TabWidget import TabWidget
+from CodeEditor import CodeEditor
 
 __author__ = "Rohan Kishore (@rohankishore on GitHub)"
 __copyright__ = "Copyright (C) 2023 Rohan Kishore"
 __license__ = "MIT License"
-__version__ = "2.0"
+__version__ = "2.1"
 
-tab_stylesheet = """ ........0
+#config_data
+nfs = Preferences.new_file_shortcut
+ofs = Preferences.open_file_shortcut
+sfs = Preferences.save_file_shortcut
+cfs = Preferences.close_file_shortcut
+font = Preferences.font
+font_size = Preferences.font_size
+
+tab_stylesheet = """ ......5..0 
     QTabBar::tab:selected {background: white;}
     QTabWidget::pane{ border: 0;}
     QTabBar::tab:hover {background-color: #d5d5d5; color: black;}
     """
 
-
 class Window(QMainWindow):
     def __init__(self, parent=None):
-        super().__init__()
+        super(Window, self).__init__()
 
-        splash_pix = QPixmap("splash.png")
+        splash_pix = QPixmap("Icons/splash.png")
         splash = QSplashScreen(splash_pix)
         splash.show()
         time.sleep(0.5)
         splash.hide()
 
         self.setWindowTitle("Aura Text")
-        self.setWindowIcon(QtGui.QIcon("icon.ico"))
+        self.setWindowIcon(QtGui.QIcon("Icons/icon.ico"))
         self.current_editor = self.create_editor()
         self.editors = []
         self.tab_widget = TabWidget()
@@ -65,52 +59,45 @@ class Window(QMainWindow):
                     background-color: #1d1d1d;
                     border: 1px solid #000;
                 }
-
                 QMenuBar::item {
                     background-color: #1d1d1d;
                     color: rgb(255,255,255);
                 }
-
                 QMenuBar::item::selected {
                     background-color: #1b1b1b;
                 }
-
                 QMenu {
                     background-color: rgb(49,49,49);
                     color: rgb(255,255,255);
-                    border: 1px solid #000;
+                    border: 1px solid #000;           
                 }
-
                 QMenu::item::selected {
                     background-color: rgb(30,30,30);
                 }
-            """,
+            """
         )
 
         menubar_items = {
             "&File": [
-                ("&New       ", "Ctrl+N", self.new_document),
-                ("&Open", "Ctrl+O", self.open_document),
-                ("&Save", "Ctrl+S", self.save_document),
+                ("New File       ", "Ctrl+N", self.new_document),
+                ("Open File", "Ctrl+O", self.open_document),
+                ("Save As", "Ctrl+S", self.save_document),
                 None,
-                ("&Summary", None, self.summary),
+                ("Summary", None, self.summary),
                 None,
-                ("&Quit", "Alt+F4", self.quit),
+                ("Exit", "Alt+F4", self.quit),
             ],
             "&Edit": [
-                ("&Cut    ", "Ctrl+X", self.cut_document),
-                ("&Copy", "Ctrl+C", self.copy_document),
-                ("&Paste", "Ctrl+V", self.paste_document),
-                ("&Undo", "Ctrl+Z", self.undo_document),
-                ("&Redo", "Ctrl+Y", self.redo_document),
+                ("Cut Selection    ", "Ctrl+X", self.cut_document),
+                ("Copy Selection", "Ctrl+C", self.copy_document),
+                ("Paste", "Ctrl+V", self.paste_document),
+                ("Undo", "Ctrl+Z", self.undo_document),
+                ("Redo", "Ctrl+Y", self.redo_document),
                 None,
+                ("Duplicate Line", None, self.duplicate_line),
             ],
             "&Code": [
-                ("&Generate        ", "Alt + Insert", None),
-                ("&Code Formatting", None, self.code_formatting),
-                None,
-                ("&Make Code Snippet", None, self.save_snippet),
-                ("&Load Code Snippet", None, self.load_snippet),
+                ("Code Formatting", None, self.code_formatting),
                 None,
             ],
             "&View": [("&Fullscreen       ", "F11", self.fullscreen), None],
@@ -118,16 +105,21 @@ class Window(QMainWindow):
                 ("Read Aloud     ", None, self.speak),
                 ("Upload to Pastebin", None, self.pastebin),
             ],
+
+            "&Prefernces": [
+                ("Settings     ", None, self.settings_at),
+                ("Themeing", None, self.settings_at),
+            ],
+
             "&Help": [
-                ("About", None, self.version),
-                ("GitHub", None, self.about_github),
-                ("Buy Me A Coffee", None, self.buymeacoffee),
-                None,
                 ("Getting Started", None, self.getting_started),
                 ("Submit a Bug Report", None, self.bug_report),
                 None,
-                ("Join Discord Server", None, self.discord),
+                ("GitHub", None, self.about_github),
                 ("Contribute to Aura Text", None, self.contrib),
+                ("Join Discord Server", None, self.discord),
+                ("Buy Me A Coffee", None, self.buymeacoffee),
+                ("About", None, self.version),
             ],
         }
 
@@ -147,24 +139,40 @@ class Window(QMainWindow):
         if index < len(self.editors):
             del self.editors[index]
 
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, 'Save File',random.choice(ModuleFile.emsg_save_list),
+            QMessageBox.Save | QMessageBox.StandardButton.Discard | QMessageBox.StandardButton.Cancel, QMessageBox.Save)
+
+        if reply == QMessageBox.Save:
+            self.save_document()
+            event.accept()
+        elif reply == QMessageBox.StandardButton.Discard:
+            event.accept()
+        else:
+            event.ignore()
+
     def create_editor(self):
         self.text_editor = CodeEditor()
-        self.text_editor.setFont(QFont("Consolas", 11))
-        highlighter = PythonHighlighter(self.text_editor.document())
+        self.text_editor.setFont(QFont(font, font_size))
+        if Preferences.syntax_highlighting == "on":
+            highlighter = PythonHighlighter(Preferences.programming_language, self.text_editor.document())
+        else:
+            pass
+
         self.text_editor.setStyleSheet(
-            "QPlainTextEdit {background-color: #1e1f22; color: white;}",
+            "QPlainTextEdit {background-color: #1e1f22; color: white;}"
         )
         self.text_editor.setTabStopDistance(12)
 
         # bindings
-        QShortcut(QKeySequence("Ctrl+N"), self.text_editor).activated.connect(
-            self.new_document,
+        QShortcut(QKeySequence(nfs), self.text_editor).activated.connect(
+            self.new_document
         )
-        QShortcut(QKeySequence("Ctrl+O"), self.text_editor).activated.connect(
-            self.open_document,
+        QShortcut(QKeySequence(ofs), self.text_editor).activated.connect(
+            self.open_document
         )
-        QShortcut(QKeySequence("Ctrl+S"), self.text_editor).activated.connect(
-            self.save_document,
+        QShortcut(QKeySequence(sfs), self.text_editor).activated.connect(
+            self.save_document
         )
         return self.text_editor
 
@@ -186,29 +194,14 @@ class Window(QMainWindow):
 
     def contrib(self):
         webbrowser.open_new_tab(
-            "https://github.com/rohankishore/Aura-Text/blob/main/CONTRIBUTING.md",
+            "https://github.com/rohankishore/Aura-Text/blob/main/CONTRIBUTING.md"
         )
 
     def open_document(self):
-        file_dir = filedialog.askopenfilename(
-            title="Select file",
-        )
-        if file_dir:
-            try:
-                path = Path(file_dir)
-                self.new_document(title=path.as_uri())
-                self.current_editor.setPlainText(path.read_text())
-            except FileNotFoundError:
-                return
+        ModuleFile.open_document(self)
 
     def save_document(self):
         ModuleFile.save_document(self)
-
-    def load_snippet(self):
-        ModuleFile.load_snippet(self)
-
-    def save_snippet(self):
-        ModuleFile.save_snippet(self)
 
     def speak(self):
         sel = self.current_editor.toPlainText()
@@ -218,21 +211,7 @@ class Window(QMainWindow):
         ModuleFile.summary(self)
 
     def code_formatting(self):
-        og_code = str(self.current_editor.toPlainText())
-        if og_code:
-            options = {
-                "aggressive": 1,
-                "experimental": True,
-            }
-            clean_code = autopep8.fix_code(og_code, options=options)
-            self.custom_new_document(title="Code Formatting")
-            self.current_editor.setPlainText(clean_code)
-        else:
-            messagebox.showerror(
-                "Error: No Code Found!",
-                "It looks like your keyboard is on vacation. Please wake it up and "
-                "start typing some code so we can work our magic.",
-            )
+        ModuleFile.code_formatting(self)
 
     def pastebin(self):
         ModuleFile.pastebin(self)
@@ -252,12 +231,11 @@ class Window(QMainWindow):
     def copy_document(self):
         self.current_editor.copy()
 
+    def duplicate_line(self):
+        ModuleFile.duplicate_line(self)
+
     def paste_document(self):
         self.current_editor.paste()
-
-    def add_indent(self):
-        if self.text_editor.toPlainText().endswith(":\n"):
-            self.text_editor.insertPlainText("    ")
 
     def fullscreen(self):
         if not self.isFullScreen():
@@ -272,13 +250,10 @@ class Window(QMainWindow):
     @staticmethod
     def version():
         text_ver = (
-            "Aura Text"  # noqa: ISC003
+            "Aura Text"
             + "\n"
             + "Current Version: "
-            + "2.0"
-            + "\n"
-            + "\n"
-            + "Hello PySide6!!"
+            + "2.1"
             + "\n"
             + "\n"
             + "Copyright © 2023 Rohan Kishore."
@@ -294,22 +269,26 @@ class Window(QMainWindow):
     def buymeacoffee():
         webbrowser.open_new_tab("https://www.buymeacoffee.com/auratext")
 
+    def settings_at(self):
+        ModuleFile.open_custom_document(self, file_dir="Preferences.py")
+
+    def themeing_at(self):
+        ModuleFile.open_custom_document(self, file_dir="themeing.py")
+
     @staticmethod
     def bug_report():
         webbrowser.open_new_tab(
-            "https://github.com/rohankishore/Aura-Text/issues/new/choose",
+            "https://github.com/rohankishore/Aura-Text/issues/new/choose"
         )
 
     @staticmethod
     def discord():
         webbrowser.open_new_tab("https://discord.gg/4PJfTugn")
 
-
 def run():
     app = QApplication(sys.argv)
     GUI = Window()
     GUI.setGeometry(200, 100, 1300, 750)
     sys.exit(app.exec())
-
 
 run()
