4f7b22a5f53d88ccb55a1722174e8684aadddd61
Rohan Kishore <109947257+rohankishore@users.noreply.github.com>
2023-05-04 12:31:01 +0530

    v2.5

--- auratext/CodeEditor.py
+++ auratext/CodeEditor.py
@@ -1,14 +1,23 @@
-import ModuleFile
 from PySide6 import QtGui
-from PySide6.QtCore import QRect
-from PySide6.QtCore import Qt
-from PySide6.QtGui import QColor
-from PySide6.QtGui import QPainter
-from PySide6.QtGui import QTextFormat
-from PySide6.QtWidgets import QMenu
-from PySide6.QtWidgets import QPlainTextEdit
-from PySide6.QtWidgets import QTextEdit
-from PySide6.QtWidgets import QWidget
+from PySide6.QtCore import Qt, QRect, Signal
+from PySide6.QtWidgets import QWidget, QPlainTextEdit, QMenu, QTextEdit, QCompleter
+from PySide6.QtGui import QPainter, QColor, QTextFormat, QTextCursor
+import Modules as ModuleFile
+
+class Completer(QCompleter):
+    insertText = Signal(str)
+
+    def __init__(self, myKeywords, parent=None):
+        QCompleter.__init__(self, myKeywords, parent)
+        self.activated.connect(self.changeCompletion)
+
+    def changeCompletion(self, completion):
+        if completion.find("(") != -1:
+            completion = completion[:completion.find("(")]
+            print(completion)
+        print("completion is " + str(completion))
+        self.insertText.emit(completion + " ")
+        self.popup().hide()
 
 
 class LineNumberArea(QWidget):
@@ -22,7 +31,7 @@ class LineNumberArea(QWidget):
 
 
 class CodeEditor(QPlainTextEdit):
-    def __init__(self, parent=None):
+    def __init__(self, keyword, parent=None):
         QPlainTextEdit.__init__(self, parent)
         self.lineNumberArea = LineNumberArea(self)
         self.blockCountChanged.connect(self.updateLineNumberAreaWidth)
@@ -30,39 +39,84 @@ class CodeEditor(QPlainTextEdit):
         self.cursorPositionChanged.connect(self.highlightCurrentLine)
         self.updateLineNumberAreaWidth(0)
         self.highlightCurrentLine()
+        completer = QCompleter(keyword)
+        completer.activated.connect(self.insert_completion)
+        completer.setWidget(self)
+        completer.setCompletionMode(QCompleter.CompletionMode.PopupCompletion)
+        completer.setCaseSensitivity(Qt.CaseSensitivity.CaseInsensitive)
+        self.completer = completer
+        self.textChanged.connect(self.complete)
+
+    def insert_completion(self, completion):
+        tc = self.textCursor()
+        extra = len(completion) - len(self.completer.completionPrefix())
+        tc.movePosition(QTextCursor.MoveOperation.Left)
+        tc.movePosition(QTextCursor.MoveOperation.EndOfWord)
+        tc.insertText(completion[-extra:] + " ")
+        self.setTextCursor(tc)
+
+    @property
+    def text_under_cursor(self):
+        tc = self.textCursor()
+        tc.select(QTextCursor.SelectionType.WordUnderCursor)
+        return tc.selectedText()
+
+    def complete(self):
+        prefix = self.text_under_cursor
+        self.completer.setCompletionPrefix(prefix)
+        popup = self.completer.popup()
+        popup.setStyleSheet("background-color : #2b2d30; color: white")
+        popup.setFixedWidth(150)
+        cr = self.cursorRect()
+        popup.setCurrentIndex(self.completer.completionModel().index(0, 0))
+        cr.setWidth(
+            self.completer.popup().sizeHintForColumn(0)
+            + self.completer.popup().verticalScrollBar().sizeHint().width()
+        )
+        self.completer.complete(cr)
+
+    def keyPressEvent(self, event: QtGui.QKeyEvent) -> None:
+        if self.completer.popup().isVisible() and event.key() in [
+            #Qt.Key.Key_Enter,
+            Qt.Key.Key_Up,
+            Qt.Key.Key_Down,
+            Qt.Key.Key_Tab,
+            Qt.Key.Key_Backtab,
+        ]:
+            event.ignore()
+            return
+        super().keyPressEvent(event)
 
     def lineNumberAreaPaintEvent(self, event):
         painter = QPainter(self.lineNumberArea)
         painter.fillRect(event.rect(), "#1e1f22")
         block = self.firstVisibleBlock()
         blockNumber = block.blockNumber()
-        top = self.blockBoundingGeometry(block).translated(self.contentOffset()).top()
+        top = self.blockBoundingGeometry(
+            block).translated(self.contentOffset()).top()
         bottom = top + self.blockBoundingRect(block).height()
 
         while block.isValid() and top <= event.rect().bottom():
             if block.isVisible() and bottom >= event.rect().top():
                 number = str(blockNumber + 1)
                 painter.setPen(Qt.white)
-                painter.drawText(
-                    0,
-                    top,
-                    self.lineNumberArea.width(),
-                    self.fontMetrics().height(),
-                    Qt.AlignmentFlag.AlignCenter,
-                    number,
-                )
+                painter.drawText(0, top, self.lineNumberArea.width(),
+                                 self.fontMetrics().height(),
+                                 Qt.AlignmentFlag.AlignCenter, number)
             block = block.next()
             top = bottom
             bottom = top + self.blockBoundingRect(block).height()
             blockNumber += 1
 
     def lineNumberAreaWidth(self):
-        digits = len(str(self.blockCount()))
-        space = 50 + 2 * digits
+        space = 50 + 5
         return space
 
     def encrypt(self):
         ModuleFile.encypt(self)
+
+    def search_google(self):
+        ModuleFile.search_google(self)
 
     def decrypt(self):
         ModuleFile.decode(self)
@@ -75,20 +129,21 @@ class CodeEditor(QPlainTextEdit):
 
         encrypt_menu = QMenu(contextMenu)
         encrypt_menu.setTitle("Encryption")
-        encrypt_menu.addAction("Encrypt", self.encrypt)
-        encrypt_menu.addAction("Decrypt", self.decrypt)
+        encrypt_menu.addAction("Encrypt Selection", self.encrypt)
+        encrypt_menu.addAction("Decrypt Selection", self.decrypt)
 
         contextMenu.addAction("Undo", self.undo).setShortcut("Ctrl+Z")
         contextMenu.addAction("Redo", self.redo).setShortcut("Ctrl+Shift+Z")
         contextMenu.addSeparator()
-        contextMenu.addAction("Select All", self.selectAll).setShortcut("Ctrl+A")
+        contextMenu.addAction("Select All",
+                              self.selectAll).setShortcut("Ctrl+A")
         contextMenu.addAction("Cut", self.cut).setShortcut("Ctrl+X")
         contextMenu.addAction(
             "Copy                                  ",
-            self.copy,
-        ).setShortcut("Ctrl+C")
+            self.copy).setShortcut("Ctrl+C")
         contextMenu.addAction("Paste", self.paste).setShortcut("Ctrl+V")
         contextMenu.addSeparator()
+        contextMenu.addAction("Search In Google", self.search_google)
         contextMenu.addAction("Calculate", self.calculate)
         contextMenu.addMenu(encrypt_menu)
         action = contextMenu.exec(self.mapToGlobal(event.pos()))
@@ -97,10 +152,13 @@ class CodeEditor(QPlainTextEdit):
         QPlainTextEdit.resizeEvent(self, event)
         cr = self.contentsRect()
         self.lineNumberArea.setGeometry(
-            QRect(cr.left(), cr.top(), self.lineNumberAreaWidth(), cr.height()),
-        )
+            QRect(
+                cr.left(),
+                cr.top(),
+                self.lineNumberAreaWidth(),
+                cr.height()))
 
-    def updateLineNumberAreaWidth(self, _):
+    def updateLineNumberAreaWidth(self, newBlockCount):
         self.setViewportMargins(self.lineNumberAreaWidth(), 0, 0, 0)
 
     def highlightCurrentLine(self):
@@ -122,10 +180,8 @@ class CodeEditor(QPlainTextEdit):
             self.lineNumberArea.scroll(0, dy)
         else:
             self.lineNumberArea.update(
-                0,
-                rect.y(),
+                0, rect.y(),
                 self.lineNumberArea.width(),
-                rect.height(),
-            )
+                rect.height())
         if rect.contains(self.viewport().rect()):
             self.updateLineNumberAreaWidth(0)
