50544a192d0e6ff5ec42503dc99ad092cb255c94
Rohan Kishore <109947257+rohankishore@users.noreply.github.com>
2024-02-06 13:23:51 +0530

    #63 add material theming support

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
+import json
+import os
+
 from PyQt6.QtWidgets import QApplication
 import sys
 
+from qt_material import apply_stylesheet
+
 from auratext.Core.window import Window
+from auratext.Core import get_started
+
+""" 
+This file includes the code to run the app. It also scans if the app is being opened for the first time in order to show the
+setup instructions.
+"""
+
+local_app_data = os.path.join(os.getenv("LocalAppData"), "AuraText")
+with open(f"{local_app_data}/data/config.json", "r") as config_file:
+    _config = json.load(config_file)
+with open(f"{local_app_data}/data/theme.json", "r") as config_file:
+    _theme = json.load(config_file)
 
 
 def main():
     app = QApplication(sys.argv)
+    if _theme["theming"] == "material":
+        theme = _theme["material_type"] + ".xml"
+        apply_stylesheet(app, theme=theme)
     ex = Window()
     sys.exit(app.exec())
 
