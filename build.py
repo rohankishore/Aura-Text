e6578869931a299221e093b0c2ddc37f2ccbcd53
Matthew Yang <matthew.yang204@gmail.com>
2025-08-26 21:04:01 -0700

    Add a build script

---
+++ build.py
@@ -0,0 +1,37 @@
+import subprocess
+import sys
+import os
+import platform
+
+def run_pyinstaller():
+    try:
+        main_script = 'main.py'
+
+        # PyInstaller command to build the executable
+        if platform.system() == 'Darwin':
+            cmd = [
+                'pyinstaller',
+                main_script,
+                '--onedir',  # Create a single folder
+                '-w',  # Makes it windowed
+                '--name "Aura Text"',
+                '--icon=icon.ico'
+            ]
+        else:
+            cmd = [
+                'pyinstaller',
+                main_script,
+                '--onedir',  # Create a single folder
+                '-w',  # Makes it windowed
+                '--icon=icon.ico'
+            ]
+
+        # Run PyInstaller
+        subprocess.check_call(cmd)
+
+        print("Build successful.")
+    except Exception as e:
+        print(f"Build failed: {e}")
+
+if __name__ == '__main__':
+    run_pyinstaller()
