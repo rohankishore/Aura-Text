4bc28a40fdce1ed97a974bcd9e45be52faf649a8
Matthew Yang <matthew.yang204@gmail.com>
2025-08-27 09:20:53 -0700

    Remove --onedir as it's pyinstaller's default behavior
    Anyways, this should not be present on macOS, when an app bundle should be built instead

--- build.py
+++ build.py
@@ -11,7 +11,6 @@ def run_pyinstaller():
         cmd = [
             'pyinstaller',
             main_script,
-            '--onedir',  # Create a single folder
             '-w',  # Makes it windowed
             '--name', "Aura Text",
             '--icon=icon.ico',
