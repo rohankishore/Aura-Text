ed7410f5ec8f5f08603ae71ca08b827d5d2331fd
Matthew Yang <matthew.yang204@gmail.com>
2025-08-26 20:52:29 -0700

    Copy from local directory to make portable
    Also remove a few whitespaces

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import json
 import os
+import shutil
+import platform
 
 from PyQt6.QtWidgets import QApplication
 import sys
 
 from qt_material import apply_stylesheet
+
+if platform.system() == "Windows":
+    local_app_data = os.getenv('LOCALAPPDATA')
+elif platform.system() == "Linux":
+    local_app_data = os.path.expanduser("~/.config")
+elif platform.system() == "Darwin":
+    local_app_data = os.path.expanduser("~/Library/Application Support")
+else:
+    print("Unsupported operating system")
+    sys.exit(1)
+local_app_data = os.path.join(local_app_data, "AuraText")
+print(local_app_data)
+script_dir = os.path.dirname(os.path.abspath(__file__))
+print(script_dir)
+shutil.copytree(os.path.join(script_dir, "LocalAppData", "AuraText"), local_app_data, dirs_exist_ok=True)
 
 from auratext.Core.window import Window
 # from auratext.Core import get_started
@@ -14,12 +31,10 @@ This file includes the code to run the app. It also scans if the app is being op
 setup instructions.
 """
 
-local_app_data = os.path.join(os.getenv("LocalAppData"), "AuraText")
 with open(f"{local_app_data}/data/config.json", "r") as config_file:
     _config = json.load(config_file)
 with open(f"{local_app_data}/data/theme.json", "r") as config_file:
     _theme = json.load(config_file)
-
 
 def main():
     app = QApplication(sys.argv)
@@ -28,7 +43,6 @@ def main():
         apply_stylesheet(app, theme=theme)
     ex = Window()
     sys.exit(app.exec())
-
 
 if __name__ == "__main__":
     main()
