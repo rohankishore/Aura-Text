b33fbac544b86accfaea45e66112845c6f56f1e9
Rohan Kishore <rohankishore746@outlook.com>
2026-02-21 18:33:10 +0530

    update docstrings

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ import sys
 
 from qt_material import apply_stylesheet
 
-# The app will automatically check system platform and set the local app data path accordingly. 
-# It will then load the config and theme files to apply the user's settings and theme preferences.
+"""
+The app will automatically check system platform and set the local app data path accordingly. 
+It will then load the config and theme files to apply the user's settings and theme preferences.
+"""
 
 if platform.system() == "Windows":
     local_app_data = os.getenv('LOCALAPPDATA')
