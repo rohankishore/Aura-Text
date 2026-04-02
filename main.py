fd396a4441a69b3543489626dbecd407247f74b1
Rohan Kishore <rohankishore746@outlook.com>
2026-04-03 01:32:36 +0530

    literally.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import shutil
 import platform
 
 # im lowk going insane.
+# pls forgive me for this abomination of a code. I just want to make it work and move on with my life.
 
 # Suppress fcukass Qt startup warnings.
 existing_qt_logging_rules = os.environ.get("QT_LOGGING_RULES", "").strip()
