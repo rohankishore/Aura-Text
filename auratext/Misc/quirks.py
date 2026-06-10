77f28899b6d67062ebfb2096afcbcae5a01cb018
Rohan Kishore <rohankishore746@outlook.com>
2026-06-10 22:25:12 +0530

    remove prints where not needed

--- auratext/Misc/quirks.py
+++ auratext/Misc/quirks.py
@@ -2,14 +2,14 @@ import os
 import shutil
 
 def get_linux_productname():
-    print("Getting Linux product name in order to apply quirks...", end='')
+    # print("Getting Linux product name in order to apply quirks...", end='')
     with open("/sys/class/dmi/id/product_name") as f:
         product_name = f.read().strip()
     print(product_name)
     return product_name
 
 def crosvm_quirks():
-    print("Applying Chrome OS crostini quirks...")
+    # print("Applying Chrome OS crostini quirks...")
     os.environ["QT_QPA_PLATFORM"] = "xcb"
 
 def copy_if_not_exists(src, dst, *, follow_symlinks=True):
