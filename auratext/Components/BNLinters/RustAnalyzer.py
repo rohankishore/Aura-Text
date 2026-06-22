7d6e90c8cfd79bd4173cec465d3182a65520eef2
Matthew Yang <matthew.yang204@gmail.com>
2026-06-22 11:08:11 -0700

    Create a module of bundled linters

---
+++ auratext/Components/BNLinters/RustAnalyzer.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+from .. import NonBlockingIO
+from sansio_lsp_client import Client
+
+class RustAnalyzer(QObject):
+    def __init__(self, parent=None, binaryPath=""):
+        super().__init__(parent=parent)
+        self.parent = parent
+        
+
+    def resolveFileURIFromPath(self, path):
+        pathobj = Path(path).resolve()
+        return pathobj.as_uri()
