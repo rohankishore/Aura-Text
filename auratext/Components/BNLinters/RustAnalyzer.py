67f8437a03e7d47c92d6fbdd80daf6f4d75140a6
Matthew Yang <matthew.yang204@gmail.com>
2026-06-23 09:57:10 -0700

    Write a mostly complete isolated LSP thing for Rust

--- auratext/Components/BNLinters/RustAnalyzer.py
+++ auratext/Components/BNLinters/RustAnalyzer.py
@@ -11,11 +11,87 @@ class LangServerNoExistError(Exception):
     pass
 
 class RustAnalyzer(QObject):
-    def __init__(self, parent=None, binaryPath=""):
+    def __init__(self, parent=None, binaryPath="", file_path=""):
         super().__init__(parent=parent)
         self.parent = parent
-        
+        if not binaryPath:
+            raise LangServerNoExistError("Language server path cannot be empty")
+        if not os.path.exists(binaryPath):
+            raise LangServerNoExistError(f"Language server does not exist at {binaryPath}")
+        self.rootpath = self.parent.parent.cpath
+        self.file_path = file_path
+        self.rootURI = self.resolveFileURIFromPath(self.rootpath)
+        self.fileURI = self.resolveFileURIFromPath(self.file_path)
+        self.process = subprocess.Popen(
+            [binaryPath],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
+        )
+        self.io = NonBlockingIO(self.process)
+        self.lsp = Client(trace="verbose", root_uri=self.rootURI)
+        self.lsp.initialize(
+            processId=os.getpid(),
+            rootUri=self.rootURI,
+            capabilities={}
+        )
+        self.io.write(self.lsp.send())
+        self.initialized = False
+        self.version = 1
+
+        QTimer.singleShot(0, self.IOPump)
 
     def resolveFileURIFromPath(self, path):
         pathobj = Path(path).resolve()
         return pathobj.as_uri()
+
+    def onFileOpen(self):
+        text = self.parent.text()
+        self.lsp.did_open(
+            TextDocumentItem(
+                uri=self.fileURI,
+                languageId="rust",
+                version=self.version,
+                text=text
+            )
+        )
+        self.io.write(self.lsp.send())
+
+    def onFileChange(self):
+        text = self.parent.text()
+        self.version += 1
+        self.lsp.did_change(
+            text_document=VersionedTextDocumentIdentifier(
+                uri=self.fileURI,
+                version=self.version
+            ),
+            content_changes=[
+                TextDocumentContentChangeEvent(
+                    text=text
+                )
+            ]
+        )
+        self.io.write(self.lsp.send())
+
+    def IOPump(self):
+        outgoing = self.lsp.send()
+        if outgoing:
+            self.io.write(outgoing)
+
+        incoming = self.io.read()
+        if incoming:
+            for event in self.lsp.recv(incoming):
+                if isinstance(event, Initialized):
+                    self.lsp.initialized()
+                    self.io.write(self.lsp.send())
+                    self.initialized = True
+                    self.onFileOpen()
+                self.handle_event(event)
+        elif incoming == b"":
+            print("ERROR: Rust Analyzer exited early")
+            return
+        if self.process.poll() is not None:
+            print("Process died")
+            return
+
+        QTimer.singleShot(20, self.IOPump)
