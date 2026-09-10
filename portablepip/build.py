192d5218c30f2a7c4ae1761882b6936231c20c7e
Matthew Yang <matthew.yang204@gmail.com>
2026-09-09 19:26:41 -0700

    Add a shitlist of modules we don't want in the pip build

--- portablepip/build.py
+++ portablepip/build.py
@@ -3,7 +3,32 @@ import platform
 import os
 import pip
 
+hooks_exclude_list = [
+    "--exclude-module", "IPython",
+    "--exclude-module", "numpy",
+    "--exclude-module", "matplotlib",
+    "--exclude-module", "PIL",
+    "--exclude-module", "PyQt6",
+    "--exclude-module", "zmq",
+    "--exclude-module", "jedi",
+    "--exclude-module", "parso",
+    "--exclude-module", "pygments",
+    "--exclude-module", "nbformat",
+    "--exclude-module", "jsonschema",
+    "--exclude-module", "jsonschema_specifications",
+    "--exclude-module", "lark",
+    "--exclude-module", "jinja2",
+    "--exclude-module", "wcwidth",
+    "--exclude-module", "matplotlib.backends",
+    "--exclude-module", "matplotlib.pyplot",
+    "--exclude-module", "matplotlib.backend_bases",
+    "--exclude-module", "PIL.Image",
+    "--exclude-module", "PIL.ImageFilter",
+    "--exclude-module", "PIL.SpiderImagePlugin",
+]
+
 def run_pyinstaller():
+    global hooks_exclude_list
     try:
         name = "portablepip"
         if platform.python_version().startswith('3'):
@@ -14,7 +39,8 @@ def run_pyinstaller():
             sep = ";"
         else:
             sep = ":"
-        cmd = ['pyinstaller', '--onefile', file_path, '--name', name, '--hidden-import', 'pip', '--add-data', f"{pip_path}{sep}pip"]
+        cmd = ['pyinstaller', '--onefile', file_path, '--name', name, '--hidden-import', 'pip', '--add-data', f"{pip_path}{sep}pip", "--clean"]
+        cmd.extend(hooks_exclude_list)
         subprocess.check_call(cmd)
     except Exception as e:
             print(f"Build failed: {e}")
