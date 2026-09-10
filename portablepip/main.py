5cd3f351bd2e52937e53da3f31dfaa665dd77b9f
Matthew Yang (杨佳明) <matthew.yang204@gmail.com>
2026-09-09 18:45:14 -0700

    Use sys.exit to exit

--- portablepip/main.py
+++ portablepip/main.py
@@ -4,7 +4,7 @@ from pip._internal.cli.main import main
 if "--pythonversion" in sys.argv:
     python_system_version = sys.version
     print(f"Built with Python {python_system_version}")
-    exit(0)
+    sys.exit(0)
 
 if __name__ == '__main__':
     sys.argv[0] = sys.argv[0].removesuffix('.exe')
