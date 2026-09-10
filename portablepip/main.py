830772b7bfe477d18715799c7c7b48a9b2b20bd5
Matthew Yang (杨佳明) <matthew.yang204@gmail.com>
2026-09-09 18:17:59 -0700

    Create launcher for portable pip

---
+++ portablepip/main.py
@@ -0,0 +1,11 @@
+import sys
+from pip._internal.cli.main import main
+
+if "--pythonversion" in sys.argv:
+    python_system_version = sys.version
+    print(f"Built with Python {python_system_version}")
+    exit(0)
+
+if __name__ == '__main__':
+    sys.argv[0] = sys.argv[0].removesuffix('.exe')
+    sys.exit(main())
